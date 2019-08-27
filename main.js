11cd2385879c77f216fad802309e0378d8337e7e
avinashcodemymobile <52481341+avinashcodemymobile@users.noreply.github.com>
2019-08-27 18:18:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ function currentTime() {
     var min = date.getMinutes();
     var sec = date.getSeconds();
     var midday = "AM";
-    let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][new Date().getDay()];
+    var weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][new Date().getDay()];
     midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
     hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour); /* assigning hour in 12-hour format */
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
-    let datestyle = new Date().toLocaleDateString("in", {
+    var datestyle = new Date().toLocaleDateString("in", {
         year: "numeric",
-        day: "2-digit",
-        month: "2-digit"
+        month: "2-digit",
+        day: "2-digit"
     });
     document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
     document.getElementById("date").innerText = datestyle; /* adding time to the div */
@@ -31,3 +31,8 @@ function updateTime(k) {
 }
 
 currentTime();
+$.getJSON("http://www.geoplugin.net/json.gp?jsoncallback=?", function (data) {
+    city = data.geoplugin_city;
+    document.getElementById("city").innerText = city;
+
+});
